26d682465a13d2983bfb5f95dd28a0c3ec57e18a
Arpit Palo <arpitpalo@gmail.com>
2020-08-29 12:40:14 +0530

    Update script-ec2.sh

--- script-ec2.sh
+++ script-ec2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo docker run -d --name=redis redis
-sudo docker run -d --name=db -e POSTGRES_HOST_AUTH_METHOD=trust postgres:9.4
-sudo docker run -d --name=vote -p 5000:80 --link redis:redis prabhavagrawal/vote-app
-sudo docker run -d --name=result -p 5001:80 --link redis:redis --link db:db prabhavagrawal/result-app
-sudo docker run -d --name=worker --link redis:redis --link db:db prabhavagrawal/worker-app
+docker run -d --name=redis redis
+docker run -d --name=db -e POSTGRES_HOST_AUTH_METHOD=trust postgres:9.4
+docker run -d --name=vote -p 5000:80 --link redis:redis prabhavagrawal/vote-app
+docker run -d --name=result -p 5001:80 --link redis:redis --link db:db prabhavagrawal/result-app
+docker run -d --name=worker --link redis:redis --link db:db prabhavagrawal/worker-app
